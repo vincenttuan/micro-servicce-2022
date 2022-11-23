814406861aca17de8a58bb3d822fcbf83dd61629
vincenttuan <vincentjava@yahoo.com.tw>
2022-11-23 22:48:12 +0800

    sts

--- spring_security_authentication/src/main/java/com/example/demo/config/SecurityConfig.java
+++ spring_security_authentication/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -9,7 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
+// 舊版
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
