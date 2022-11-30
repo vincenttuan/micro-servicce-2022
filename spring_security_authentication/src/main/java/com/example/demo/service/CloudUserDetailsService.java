8266e747677d6878f4c4b82e97e5d38cca9efeb2
vincenttuan <vincentjava@yahoo.com.tw>
2022-11-30 20:58:11 +0800

    ss

--- spring_security_authentication/src/main/java/com/example/demo/service/CloudUserDetailsService.java
+++ spring_security_authentication/src/main/java/com/example/demo/service/CloudUserDetailsService.java
@@ -1,9 +1,14 @@
 package com.example.demo.service;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -11,9 +16,34 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.dao.CloudUserRepository;
+import com.example.demo.model.CloudUser;
+
 @Service("userDetailsService")
 public class CloudUserDetailsService implements UserDetailsService {
-
+	@Autowired
+	private CloudUserRepository cloudUserRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Optional<CloudUser> optCloudUser = cloudUserRepository.findByUsername(username);
+		// 若無此帳號就拋出例外
+		optCloudUser.orElseThrow(() -> new UsernameNotFoundException("Username  not found!"));
+		
+		CloudUser cloudUser = optCloudUser.get();
+		List<GrantedAuthority> auths = Arrays.stream(cloudUser.getRoles().split(","))
+				.map(SimpleGrantedAuthority::new)
+				//.map(role -> new SimpleGrantedAuthority(role))
+				.collect(Collectors.toList());
+		
+		//建構子參數: username, password, enabled, accountNonExpired, credentialsNonExpired(證書), accountNonLocked, authorities
+		UserDetails userDetails = new User(cloudUser.getUsername(), cloudUser.getPassword(), 
+										   true, true, true, cloudUser.isActive(), auths);
+		return userDetails;
+	}
+	
+	
+	/*
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		// 假設資料庫的 name / pwd 如下
@@ -33,5 +63,6 @@ public class CloudUserDetailsService implements UserDetailsService {
 		UserDetails userDetails = new User(username, pwd, true, true, true, false, auths);
 		return userDetails;
 	}
+	*/
 
 }
