1ba2b3cd4936efa1a38f2ff6d19ec64d2187de09
vincenttuan <vincentjava@yahoo.com.tw>
2022-11-30 21:32:35 +0800

    ss

---
+++ spring_security_authentication/src/main/java/com/example/demo/util/JwtToken.java
@@ -0,0 +1,43 @@
+package com.example.demo.util;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Date;
+import java.util.Map;
+
+import javax.security.auth.message.AuthException;
+
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Component
+public class JwtToken {
+	// 有效期間
+	private static final long EXPIRATION_TIME = 1 * 60 * 1000; // 60 秒
+	// 自訂密鑰
+	public static final String SECRET = "MYSECRET";
+	
+	// 簽發 JWT
+	public String generateToken(Map<String, Object> claims) {
+		
+		return Jwts.builder()
+				.setClaims(claims)
+				.setExpiration(new Date(Instant.now().toEpochMilli() + EXPIRATION_TIME))
+				.signWith(SignatureAlgorithm.HS256, SECRET)
+				.compact();
+		
+	}
+	
+	// 驗證
+	public void validateToken(String token) throws AuthException {
+		try {
+			Jwts.parser()
+				.setSigningKey(SECRET)
+				.parseClaimsJws(token);
+		} catch (Exception e) {
+			throw new AuthException("無效的 JWT: " + e);
+		}
+	}
+}
