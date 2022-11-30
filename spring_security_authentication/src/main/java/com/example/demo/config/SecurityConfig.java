2052f46b0f8439c61242f34ec075bf56723bda17
vincenttuan <vincentjava@yahoo.com.tw>
2022-11-30 20:09:41 +0800

    ss

--- spring_security_authentication/src/main/java/com/example/demo/config/SecurityConfig.java
+++ spring_security_authentication/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -17,12 +17,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Bean
 	PasswordEncoder password() {
-		return new BCryptPasswordEncoder(); // 將密碼進行加密(每次加密資料並不會相同)
+		return new BCryptPasswordEncoder(); // 將密碼進行加密(每次加密資料並不會相同) 強度可以輸入 4~31
 	}
 	
 	// 配置身份驗證
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		System.out.println("configure(AuthenticationManagerBuilder auth)");
 		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 		String password = passwordEncoder.encode("1234");
 		auth.inMemoryAuthentication()
@@ -38,13 +39,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	// 配置 HTTP 安全性
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		System.out.println("configure(HttpSecurity http)");
 		http.authorizeHttpRequests() // 授權請求
 			// 設定放行名單
 			//.antMatchers(HttpMethod.GET, "/admin").hasAuthority("ROLE_ADMIN") // ADMIN -> ROLE_ADMIN
 			.antMatchers("/admin").hasRole("ADMIN")
 			.antMatchers("/user").hasAnyRole("USER", "ADMIN")
-			//.anyRequest().permitAll() // 其他請求皆開放
-			.anyRequest().authenticated() // 所有請求都要驗證
+			.anyRequest().permitAll() // 其他請求皆開放
+			//.anyRequest().authenticated() // 所有請求都要驗證
 			.and().formLogin(); // 利用表單來登入
 		
 		http.rememberMe() // 不會因為瀏覽器關閉而消失登入狀態
@@ -55,6 +57,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	// 配置網路安全
 	@Override
 	public void configure(WebSecurity web) throws Exception {
+		System.out.println("configure(WebSecurity web)");
 		// 不需要驗證的路徑
 		web.ignoring().antMatchers("/css/**", "/images/**", "/js/**");
 	}
---
+++ spring_security_authentication/src/main/java/com/example/demo/service/CloudUserDetailsService.java
@@ -0,0 +1,35 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service("userDetailsService")
+public class CloudUserDetailsService implements UserDetailsService {
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// 假設資料庫的 name / pwd 如下
+		String name = "john";
+		String pwd  = new BCryptPasswordEncoder().encode("1234");
+		
+		if(!username.equals(name)) {
+			System.out.println("Usernme nnot found");
+			throw new UsernameNotFoundException("Usernme nnot found");
+		}
+		
+		// 建立 UserDetials
+		List<GrantedAuthority> auths = AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_ADMIN,ROLE_USER");
+		UserDetails userDetails = new User(username, pwd, auths);
+		
+		return userDetails;
+	}
+
+}
