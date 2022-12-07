e8f3855fe46c9740a599d25929d109567dd1ec00
vincenttuan <vincentjava@yahoo.com.tw>
2022-12-07 19:52:08 +0800

    sts

---
+++ spring_security_oauth2-4mode/src/main/java/com/example/demo/SpringSecurityOauth24modeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOauth24modeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityOauth24modeApplication.class, args);
+	}
+
+}
