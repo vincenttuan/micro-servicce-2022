4661d15b81ab7b2a676fedf2e3054bb98493c59c
vincenttuan <vincentjava@yahoo.com.tw>
2022-12-14 21:38:56 +0800

    batch

---
+++ spring_batch_basic/src/main/java/com/example/demo/SpringBatchBasicApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchBasicApplication.class, args);
+	}
+
+}
