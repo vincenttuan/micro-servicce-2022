cb9cacbe60d46624eb28012706f0784aae54933b
vincenttuan <vincentjava@yahoo.com.tw>
2022-12-14 21:53:11 +0800

    batch

--- spring_batch_basic/src/main/java/com/example/demo/config/JobConfig_1_basic.java
+++ spring_batch_basic/src/main/java/com/example/demo/config/JobConfig_1_basic.java
@@ -1,6 +1,16 @@
 package com.example.demo.config;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /*
@@ -8,5 +18,30 @@ import org.springframework.context.annotation.Configuration;
  * */
 @Configuration
 public class JobConfig_1_basic {
-
+	// 透過 Job 任務來執行批次作業
+	// 要注入創建一個 JobBuilderFactory 物件用來產生 Job
+	@Autowired
+	private JobBuilderFactory jobBuilderFactory;
+	
+	// Job 任務的執行必須由 Step 決定
+	// 要注入創建一個 StepBuilderFactory 物件用來產生 Step
+	@Autowired
+	private StepBuilderFactory stepBuilderFactory;
+	
+	@Bean
+	public Job helloJob() {
+		return jobBuilderFactory.get("HelloJob")
+				.start(step1())
+				.build();
+	}
+	
+	public Step step1() {
+		return stepBuilderFactory.get("Step1")
+				.tasklet((contribution, chunkContext) -> {
+					System.out.println("Hello batch step1");
+					return RepeatStatus.FINISHED;
+				})
+				.build();
+	}
+	
 }
