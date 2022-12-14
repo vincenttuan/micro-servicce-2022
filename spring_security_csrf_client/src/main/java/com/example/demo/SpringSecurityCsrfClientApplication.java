c3b1f797710ae6db970574e2d2c8b59001ddd6d6
vincenttuan <vincentjava@yahoo.com.tw>
2022-12-14 20:14:30 +0800

    csrf

---
+++ spring_security_csrf_client/src/main/java/com/example/demo/SpringSecurityCsrfClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityCsrfClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityCsrfClientApplication.class, args);
+	}
+
+}
