6eb428f72252723ad33bfbd5a84d1076d1ce6444
vincenttuan <vincentjava@yahoo.com.tw>
2022-12-14 21:43:42 +0800

    batch

--- spring_batch_basic/src/main/java/com/example/demo/SpringBatchBasicApplication.java
+++ spring_batch_basic/src/main/java/com/example/demo/SpringBatchBasicApplication.java
@@ -1,9 +1,11 @@
 package com.example.demo;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing // 啟動批次處理
 public class SpringBatchBasicApplication {
 
 	public static void main(String[] args) {
---
+++ spring_batch_basic/src/main/java/com/example/demo/config/JobConfig_1_basic.java
@@ -0,0 +1,12 @@
+package com.example.demo.config;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.context.annotation.Configuration;
+
+/*
+ * 了解 Job 與 Step 的操作
+ * */
+@Configuration
+public class JobConfig_1_basic {
+
+}
