c2f8b9d0a167dacc7f44e39a000d5ed6f0989005
vincenttuan <vincentjava@yahoo.com.tw>
2022-12-21 19:22:27 +0800

    batch

--- spring_batch_basic/src/main/java/com/example/demo/config/JobConfig_2_flow.java
+++ spring_batch_basic/src/main/java/com/example/demo/config/JobConfig_2_flow.java
@@ -3,9 +3,10 @@ package com.example.demo.config;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.FlowBuilder;
+import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
@@ -13,41 +14,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-/*
- * 了解 Job 與 Step 的操作
- * */
-//@Configuration
-public class JobConfig_1_basic {
-	// 透過 Job 任務來執行批次作業
-	// 要注入創建一個 JobBuilderFactory 物件用來產生 Job
+// Flow 的創建與使用
+// Flow 是多個 Step 的集合，可以被多個 Job 重複使用
+@Configuration
+public class JobConfig_2_flow {
+	
 	@Autowired
 	private JobBuilderFactory jobBuilderFactory;
 	
-	// Job 任務的執行必須由 Step 決定
-	// 要注入創建一個 StepBuilderFactory 物件用來產生 Step
 	@Autowired
 	private StepBuilderFactory stepBuilderFactory;
-	
-	@Bean
-	public Job helloJob() {
-		return jobBuilderFactory.get("HelloJob")
-				.start(step1())
-				.build();
-	}
-	
-	@Bean
-	public Job helloJob2() {
-		return jobBuilderFactory.get("HelloJob2")
-				.start(step2())
-				.next(step1())
-				.build();
-	}
 	
 	@Bean
 	public Step step1() {
 		return stepBuilderFactory.get("Step1")
 				.tasklet((contribution, chunkContext) -> {
-					System.out.println("Hello batch step1");
+					System.out.println("Hello Step1");
 					return RepeatStatus.FINISHED;
 				})
 				.allowStartIfComplete(true)
@@ -58,11 +40,42 @@ public class JobConfig_1_basic {
 	public Step step2() {
 		return stepBuilderFactory.get("Step2")
 				.tasklet((contribution, chunkContext) -> {
-					System.out.println("Hello batch step2");
+					System.out.println("Hello Step2");
 					return RepeatStatus.FINISHED;
 				})
 				.allowStartIfComplete(true)
 				.build();
 	}
+	
+	@Bean
+	public Step step3() {
+		return stepBuilderFactory.get("Step3")
+				.tasklet((contribution, chunkContext) -> {
+					System.out.println("Hello Step3");
+					return RepeatStatus.FINISHED;
+				})
+				.allowStartIfComplete(true)
+				.build();
+	}
+	
+	// 建立 Floe（包含：step1()、step2() ...）
+	@Bean
+	public Flow flowDemoFlow() {
+		return new FlowBuilder<Flow>("FlowDemoFlow")
+				.start(step1())
+				.next(step2())
+				.build();
+	}
+	
+	// 建立 Job 用來執行 flow 與 step
+	@Bean
+	public Job flowDemoJob() {
+		return jobBuilderFactory.get("FlowDemoJob")
+				.start(flowDemoFlow()) // 先執行 flow 裡面的 step ...
+				.next(step3()) // 再執行 step3()
+				.end()
+				.build();
+	}
+	
 	
 }
