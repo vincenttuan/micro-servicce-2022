da803ab79c7170ca0d2c8cc72a322b84157a0900
vincenttuan <vincentjava@yahoo.com.tw>
2022-12-21 19:46:16 +0800

    batch

--- spring_batch_basic/src/main/java/com/example/demo/config/JobConfig_3_decider.java
+++ spring_batch_basic/src/main/java/com/example/demo/config/JobConfig_3_decider.java
@@ -4,17 +4,17 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.job.builder.FlowBuilder;
-import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-// Flow 的創建與使用
-// Flow 是多個 Step 的集合，可以被多個 Job 重複使用
-//@Configuration
-public class JobConfig_2_flow {
+// 透過from(...).on(xxx).to(...) 來進行簡單決策
+// on("COMPLETED")、on("FAILED")
+// 另外可以透過決策器 JobExecutionDecider 來進行較為複雜的決策
+// docs: https://docs.spring.io/spring-batch/docs/current/reference/html/step.html#programmaticFlowDecisions
+@Configuration
+public class JobConfig_3_decider {
 	
 	@Autowired
 	private JobBuilderFactory jobBuilderFactory;
@@ -26,8 +26,8 @@ public class JobConfig_2_flow {
 	public Step step1() {
 		return stepBuilderFactory.get("Step1")
 				.tasklet((contribution, chunkContext) -> {
-					System.out.println("Hello Step1");
-					return RepeatStatus.FINISHED;
+					System.out.println("Hello step1");
+					return  RepeatStatus.FINISHED;
 				})
 				.allowStartIfComplete(true)
 				.build();
@@ -37,8 +37,9 @@ public class JobConfig_2_flow {
 	public Step step2() {
 		return stepBuilderFactory.get("Step2")
 				.tasklet((contribution, chunkContext) -> {
-					System.out.println("Hello Step2");
-					return RepeatStatus.FINISHED;
+					System.out.println("Hello step2");
+					System.out.println(10/0);  // 模擬發生錯誤
+					return  RepeatStatus.FINISHED;
 				})
 				.allowStartIfComplete(true)
 				.build();
@@ -48,29 +49,27 @@ public class JobConfig_2_flow {
 	public Step step3() {
 		return stepBuilderFactory.get("Step3")
 				.tasklet((contribution, chunkContext) -> {
-					System.out.println("Hello Step3");
-					return RepeatStatus.FINISHED;
+					System.out.println("Hello step3");
+					return  RepeatStatus.FINISHED;
 				})
 				.allowStartIfComplete(true)
 				.build();
 	}
 	
-	// 建立 Floe（包含：step1()、step2() ...）
 	@Bean
-	public Flow flowDemoFlow() {
-		return new FlowBuilder<Flow>("FlowDemoFlow")
+	public Job deciderJobDemo() {
+		/*
+		return jobBuilderFactory.get("DeciderJobDemo")
 				.start(step1())
 				.next(step2())
+				.next(step3())
 				.build();
-	}
-	
-	// 建立 Job 用來執行 flow 與 step
-	@Bean
-	public Job flowDemoJob() {
-		return jobBuilderFactory.get("FlowDemoJob")
-				.start(flowDemoFlow()) // 先執行 flow 裡面的 step ...
-				.next(step3()) // 再執行 step3()
-				.end()
+		*/		
+		// 透過 on(在什麼情況下).to(做什麼事)
+		return jobBuilderFactory.get("DeciderJobDemo")
+				.start(step1()).on("COMPLETED").to(step2())
+				.from(step2()).on("FAILED").to(step3())
+				.from(step3()).end()
 				.build();
 	}
 	
