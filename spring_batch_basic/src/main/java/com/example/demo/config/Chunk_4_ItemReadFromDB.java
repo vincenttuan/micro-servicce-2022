7c49684c089f8299f9f57aa0054feb405b7257fc
vincenttuan <vincentjava@yahoo.com.tw>
2022-12-28 19:31:01 +0800

    batch

--- spring_batch_basic/src/main/java/com/example/demo/config/Chunk_4_ItemReadFromDB.java
+++ spring_batch_basic/src/main/java/com/example/demo/config/Chunk_4_ItemReadFromDB.java
@@ -1,8 +1,13 @@
 package com.example.demo.config;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.database.JdbcPagingItemReader;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 // JdbcPagingItemReader 分頁讀取
@@ -15,5 +20,30 @@ public class Chunk_4_ItemReadFromDB {
 	@Autowired
 	private StepBuilderFactory  stepBuilderFactory;
 	
+	@Autowired
+	@Qualifier("jdbcUserReader")
+	private JdbcPagingItemReader<User> jdbcUserReader;
+	
+	@Bean
+	public Job itemReaderDbJob() {
+		return jobBuilderFactory.get("ItemReaderDbJob")
+				.start(itemReaderDbStep())
+				.build();
+	}
+	
+	@Bean
+	public Step itemReaderDbStep() {
+		return stepBuilderFactory.get("ItemReaderDbStep")
+				.<User, User>chunk(2)
+				.reader(jdbcUserReader)
+				.writer(users -> {
+					for(User user : users) {
+						System.out.println(user);
+					}
+					System.out.println("--------------------------------------");
+				})
+				.allowStartIfComplete(true)
+				.build();
+	}
 	
 }
