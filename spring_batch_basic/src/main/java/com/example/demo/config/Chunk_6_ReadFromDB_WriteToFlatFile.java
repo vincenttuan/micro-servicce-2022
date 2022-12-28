f083e76cc96692d27b12263167430e66e1e8e887
vincenttuan <vincentjava@yahoo.com.tw>
2022-12-28 20:43:45 +0800

    batch

--- spring_batch_basic/src/main/java/com/example/demo/config/Chunk_6_ReadFromDB_WriteToFlatFile.java
+++ spring_batch_basic/src/main/java/com/example/demo/config/Chunk_6_ReadFromDB_WriteToFlatFile.java
@@ -1,8 +1,14 @@
 package com.example.demo.config;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.database.JdbcPagingItemReader;
+import org.springframework.batch.item.file.FlatFileItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /*
@@ -18,5 +24,32 @@ public class Chunk_6_ReadFromDB_WriteToFlatFile {
 	@Autowired
 	private StepBuilderFactory  stepBuilderFactory;
 	
+	@Autowired
+	@Qualifier("jdbcCustomerReader")
+	private JdbcPagingItemReader<Customer> jdbcCustomerReader;
+	
+	@Autowired
+	@Qualifier("flatFileCustomerWriter")
+	private FlatFileItemWriter<Customer> flatFileCustomerWriter;
+	
+	@Bean
+	public Job writeToFlatFileJob() {
+		return jobBuilderFactory.get("WriteToFlatFileJob")
+				.start(writeToFlatFileStep())
+				.build();
+	}
+	
+	@Bean
+	public Step writeToFlatFileStep() {
+		return stepBuilderFactory.get("WriteToFlatFileStep")
+				.<Customer, Customer>chunk(3)
+				.reader(jdbcCustomerReader)
+				.writer(flatFileCustomerWriter)
+				.allowStartIfComplete(true)
+				.build();
+	}
 	
 }
+
+
+
