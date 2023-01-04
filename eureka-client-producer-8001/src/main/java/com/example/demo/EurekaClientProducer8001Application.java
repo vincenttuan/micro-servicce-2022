1f5713c7330d7edf22a918af0cdd9639deb79a19
vincenttuan <vincentjava@yahoo.com.tw>
2023-01-04 20:27:58 +0800

    cloud

---
+++ eureka-client-producer-8001/src/main/java/com/example/demo/EurekaClientProducer8001Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaClientProducer8001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientProducer8001Application.class, args);
+	}
+
+}
