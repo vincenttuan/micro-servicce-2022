6bc76cb849b8d0d7544a6c075dd22d250f7def5c
vincenttuan <vincentjava@yahoo.com.tw>
2023-01-04 19:33:43 +0800

    cloud

---
+++ Producer-8001/src/main/java/com/example/demo/Producer8001Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Producer8001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Producer8001Application.class, args);
+	}
+
+}
