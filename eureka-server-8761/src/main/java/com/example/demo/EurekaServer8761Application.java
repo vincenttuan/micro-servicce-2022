8bb375e2386419d439841372acfd63c523832611
vincenttuan <vincentjava@yahoo.com.tw>
2023-01-04 20:03:28 +0800

    cloud

---
+++ eureka-server-8761/src/main/java/com/example/demo/EurekaServer8761Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServer8761Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServer8761Application.class, args);
+	}
+
+}
---
+++ eureka-server-8761/src/test/java/com/example/demo/EurekaServer8761ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServer8761ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
