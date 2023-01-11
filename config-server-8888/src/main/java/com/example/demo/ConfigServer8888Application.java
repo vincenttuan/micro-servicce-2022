57c6d30c638c023177be92dbf0d8f26dc7824315
vincenttuan <vincentjava@yahoo.com.tw>
2023-01-11 21:24:32 +0800

    config

---
+++ config-server-8888/src/main/java/com/example/demo/ConfigServer8888Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServer8888Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServer8888Application.class, args);
+	}
+
+}
