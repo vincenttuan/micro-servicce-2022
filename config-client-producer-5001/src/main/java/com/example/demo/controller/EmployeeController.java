73645eaf5fff561f92037d180b81b0206491f694
vincenttuan <vincentjava@yahoo.com.tw>
2023-01-11 21:35:36 +0800

    config

--- config-client-producer-5001/src/main/java/com/example/demo/controller/EmployeeController.java
+++ config-client-producer-5001/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,12 +11,15 @@ import com.example.demo.model.Employee;
 @RestController
 public class EmployeeController {
 	
+	@Value("${message: None}")
+	private String message;
+	
 	@GetMapping("/employee")
-	public Employee employee(HttpServletRequest request) {
+	public Employee employee() {
 		Employee emp = new Employee();
 		emp.setEmpId("1");
 		emp.setName("John");
-		emp.setDescription("Manager, port:" + request.getLocalPort());
+		emp.setDescription("message: " + message);
 		emp.setSalary(80000);
 		return emp;
 	}
