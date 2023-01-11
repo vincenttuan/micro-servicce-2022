aea46f130dd402fd88f36f283ea9ad0261f9e02d
vincenttuan <vincentjava@yahoo.com.tw>
2023-01-11 20:01:25 +0800

    zuul

---
+++ zuul-service-6070/src/main/java/com/example/demo/ZuulService6070Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulService6070Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulService6070Application.class, args);
+	}
+
+}
---
+++ zuul-service-6070/src/test/java/com/example/demo/ZuulService6070ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulService6070ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
