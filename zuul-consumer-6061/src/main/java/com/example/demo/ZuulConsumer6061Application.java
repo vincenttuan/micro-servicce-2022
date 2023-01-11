4d91bf028a98369b8d53c19696e4c60119834dbf
vincenttuan <vincentjava@yahoo.com.tw>
2023-01-11 20:16:26 +0800

    zuul

---
+++ zuul-consumer-6061/src/main/java/com/example/demo/ZuulConsumer6061Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulConsumer6061Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulConsumer6061Application.class, args);
+	}
+
+}
