dbb64beea2abf60046373da3cf860db82d048556
vincenttuan <vincentjava@yahoo.com.tw>
2023-01-11 19:18:38 +0800

    cloud

---
+++ hystrix-producer-6060/src/main/java/com/example/demo/model/Employee.java
@@ -0,0 +1,35 @@
+package com.example.demo.model;
+
+public class Employee {
+	private String empId;
+	private String name;
+	private String description;
+	private int salary;
+	
+	public String getEmpId() {
+		return empId;
+	}
+	public void setEmpId(String empId) {
+		this.empId = empId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	
+	
+}
