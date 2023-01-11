11a09b8f5fb76cfa8b45e7af771ce0c12c0aca7b
vincenttuan <vincentjava@yahoo.com.tw>
2023-01-11 20:09:06 +0800

    cloud

---
+++ zuul-producer-6062/src/main/java/com/example/demo/ZuulProducer6062Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulProducer6062Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulProducer6062Application.class, args);
+	}
+
+}
---
+++ zuul-producer-6062/src/test/java/com/example/demo/ZuulProducer6062ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulProducer6062ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
