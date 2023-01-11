04624b6b1fd4a873d5f369d6396875fcabf0e9fa
vincenttuan <vincentjava@yahoo.com.tw>
2023-01-11 19:17:18 +0800

    cloud

--- hystrix-producer-6060/src/main/java/com/example/demo/HystrixProducer6060Application.java
+++ hystrix-producer-6060/src/main/java/com/example/demo/HystrixProducer6060Application.java
@@ -2,8 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
+@EnableHystrix  // 啟動 Hystrix 機制
 public class HystrixProducer6060Application {
 
 	public static void main(String[] args) {
