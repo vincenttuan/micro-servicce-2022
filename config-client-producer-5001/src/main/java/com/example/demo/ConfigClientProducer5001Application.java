a2359e0f2b1b4e011f44154af78be228603327ca
vincenttuan <vincentjava@yahoo.com.tw>
2023-01-11 21:32:06 +0800

    config

---
+++ config-client-producer-5001/src/main/java/com/example/demo/ConfigClientProducer5001Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientProducer5001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientProducer5001Application.class, args);
+	}
+
+}
---
+++ config-client-producer-5001/src/test/java/com/example/demo/ConfigClientProducer5001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientProducer5001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
